b92a3dee88d87772918e8ba3fb500926051992f0
derek <derek.merck@ufl.edu>
2020-08-08 00:07:11 -0400

    Added duid_mint with content-based hashing

--- diana/services/redis_index.py
+++ diana/services/redis_index.py
@@ -14,16 +14,59 @@ from libsvc.endpoint import Endpoint, Serializable, UID, DataItem
 from libsvc.persistence import RedisPersistenceBackend
 from diana.dixel import Dixel
 
+KEY_SEP = "/"
+HASH_KEY_LEN = 10
+
 class KeyType(Enum):
-    UUID = auto()
+    UUID  = auto()
     MHASH = auto()
     DHASH = auto()
     BHASH = auto()
     DHUID = auto()
 
+KTy = KeyType
+
+class SlotType(Enum):
+    VALUE = "value"
+    DICT  = "dict"
+    SET   = "set"
+
+STy = SlotType
+
+
+def to_redis(dixel: Dixel, pbe: RedisPersistenceBackend):
+
+    N = 10
+    key_bhash    = f"index/b:{dixel.bhash[:N]}/v"     # -> m:dixel.mhash[:N]
+    key_children = f"index/m:{dixel.mhash[:N]}/s"     # -> set([mhashes])
+    key_data     = f"index/m:{dixel.mhash[:N]}/d"     # -> dict(main_keys, hashes)
+    key_serial   = f"index/m:{dixel.mhash[:N]}/v"     # -> pickle(object w/o data/binary)
+
+
+
 
 @attr.s(auto_attribs=True)
 class RedisIndex(Endpoint, Serializable, RedisPersistenceBackend):
+
+    namespace: str = "index"
+
+    def t(self, dixel: Dixel,
+          kty: KTy = KTy.MHASH,
+          sty: STy = STy.VALUE) -> str:
+
+        if KeyType == KeyType.MHASH:
+            h = dixel.mhash
+        elif KeyType == KeyType.DHASH:
+            h = dixel.dhash
+        elif KeyType == KeyType.BHASH:
+            h = dixel.dhash
+        else:
+            raise TypeError("Unknown key type")
+
+        s = KEY_SEP.join([self.namespace,
+                          h[:HASH_KEY_LEN],
+                          sty.value])
+        return s
 
     def status(self) -> bool:
         pass
